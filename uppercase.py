ec2b641440c0ce9216dc6ba15782068bbc32c6a4
anmoltanubhrt <33187363+anmoltanubhrt@users.noreply.github.com>
2018-10-06 15:32:49 +0530

    Update uppercase.py

--- uppercase.py
+++ uppercase.py
@@ -3,3 +3,13 @@ Write a program that accepts sequence of lines as input and prints the lines aft
 lines=input("Enter the lines you want to convert into uppercase")
 nw_lines=lines.upper()
 print(nw_lines)
+# This is another way of doing it'
+lines=[]
+while True:
+    s=raw_input()
+    if s:
+        lines.append(s.upper())
+    else:
+        break
+for sentence in lines:
+    print sentence
