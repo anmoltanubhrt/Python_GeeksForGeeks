21244b91cbce3d3112e1e15d2ad93bdfead0bb22
anmoltanubhrt <33187363+anmoltanubhrt@users.noreply.github.com>
2018-10-06 15:24:44 +0530

    Create uppercase.py

---
+++ uppercase.py
@@ -0,0 +1,5 @@
+Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
+
+lines=input("Enter the lines you want to convert into uppercase")
+nw_lines=lines.upper()
+print(nw_lines)
